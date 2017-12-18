aea667ce67d448abad8b9fd54b4da8d89b838da4
Stone <827443469@qq.com>
2017-12-18 15:12:53 +0800

    do some test

---
+++ Assets/Physic/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float Speed;
+    public float Torque;
+
+    Rigidbody _rig;
+
+	// Use this for initialization
+	void Start () {
+        _rig = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        _rig.velocity = transform.forward * Speed * v;
+        _rig.angularVelocity = transform.up * Torque * h;
+        _rig.par
+    }
+}
