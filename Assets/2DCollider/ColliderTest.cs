aa5cc18f8e8b201277779ab0b33ac68a8912bcee
Stone <827443469@qq.com>
2018-01-06 18:53:40 +0800

    2dCollider Test

---
+++ Assets/2DCollider/ColliderTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour {
+
+    public Transform Begin, End;
+    public Transform Point;
+
+	void Start () {
+		
+	}
+
+    [ContextMenu("CheckPointOnLine")]
+    void CheckPointOnLine()
+    {
+        Vector3 start = Begin.position;
+        Vector3 end = End.position;
+        Vector3 point = Point.position;
+
+        float A = end.y - start.y;
+        float B = start.x - end.x;
+        float C = end.x * start.y - start.x * end.y;
+
+        float res = A * point.x + B * point.y + C;
+        Debug.Log(res);
+    }
+}
