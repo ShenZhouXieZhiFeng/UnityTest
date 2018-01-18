ea6b127ea5123d9d74de767fe0d6c12c60820932
ShenZhouXieZhiFeng <827443469@qq.com>
2018-01-18 15:52:52 +0800

    asseret断言

---
+++ Assets/SmallCandy/AssertTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Assertions.Must;
+
+public class AssertTest : MonoBehaviour {
+
+    public bool Flag = true;
+
+    [ContextMenu("MyAssert")]
+    void MyAssert()
+    {
+        Assert.AreEqual(true,Flag, "Flag != true");
+        Debug.Log(Flag);
+    }
+}
