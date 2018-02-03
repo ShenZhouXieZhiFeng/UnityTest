1b048c37f8a686156708c86c04e9ebe37c272cdf
Stone <827443469@qq.com>
2018-02-03 20:10:30 +0800

    assetbundle 打包 依赖 加载

---
+++ Assets/AssetBundle/AssetBundleLoadTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AssetBundleLoadTest : MonoBehaviour {
+
+    string bundleUrl = "G:/Work/Project/Github/UnityTest/AssetBundles/StandaloneWindows/";
+    string cube = "cube.assetbundle";
+    string sphere = "sphere.assetbundle";
+    string mats = "mats.assetbundle";
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("LoadAsync"))
+        {
+            StartCoroutine(loadAsync());
+        }
+    }
+
+    IEnumerator loadAsync()
+    {
+        AssetBundleCreateRequest request =
+                AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(bundleUrl + cube));
+        yield return request;
+        AssetBundleCreateRequest re2 = 
+            AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(bundleUrl + mats));
+        yield return re2;
+
+        AssetBundle ab = request.assetBundle;
+
+        GameObject cubeG = ab.LoadAsset("Cube") as GameObject;
+        Instantiate(cubeG);
+    }
+}
