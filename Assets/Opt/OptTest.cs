6e38fcc0c53e2ae372dc2c20f8b51f44767febfb
Stone <827443469@qq.com>
2018-02-28 18:52:10 +0800

    dc

---
+++ Assets/Opt/OptTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptTest : MonoBehaviour {
+
+    Material _mat;
+	void Start () {
+        _mat = GetComponent<MeshRenderer>().sharedMaterial;
+	}
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("change mat"))
+        {
+            _mat.SetColor("_Color", Color.red);
+        }
+    }
+}
